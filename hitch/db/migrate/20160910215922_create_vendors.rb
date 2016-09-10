09e015704fa0b7912373e4c944fa3458cdf012ef
Luis De Castro <luisdecastro510@gmail.com>
2016-09-10 16:07:18 -0700

    add table fields to migrations

--- hitch/db/migrate/20160910215922_create_vendors.rb
+++ hitch/db/migrate/20160910215922_create_vendors.rb
@@ -1,6 +1,12 @@
 class CreateVendors < ActiveRecord::Migration[5.0]
   def change
     create_table :vendors do |t|
+    	t.string :name
+    	t.string :address
+    	t.text 	 :notes
+    	t.string :contact_name
+    	t.string :contact_phone
+    	t.string :contact_email
 
       t.timestamps
     end
--- hitch/db/migrate/20160910221649_create_proposals.rb
+++ hitch/db/migrate/20160910221649_create_proposals.rb
@@ -1,6 +1,7 @@
 class CreateProposals < ActiveRecord::Migration[5.0]
   def change
     create_table :proposals do |t|
+    	t.json :data
 
       t.timestamps
     end
